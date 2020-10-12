38e50e2e929c0ccf421f43c117656d700b5a76c3
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2020-10-12 13:24:45 +0300

    m

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -4,15 +4,24 @@ import AlertContext from '../../context/alert/alertContext';
 const Alert = () => {
     const alertContext = useContext(AlertContext);
     const {alert, remove} = alertContext;
+    
    return (
        alert !== null && (
     <div className = {`alert alert-${alert.type}`}>
         <i className="fas fa-info-circle">{alert.msg}</i>
         <button onClick={remove} style={{float:'right', width:'30px'}}>X</button>
     </div>
+        )
     )
-       )
 }
 
 
 export default Alert;
+
+
+// if (alert !==null) return ( 
+//     (<div className = {`alert alert-${alert.type}`}>
+//      <i className="fas fa-info-circle">{alert.msg}</i>
+//      <button onClick={remove} style={{float:'right', width:'30px'}}>X</button>
+//  </div>)
+//  );
