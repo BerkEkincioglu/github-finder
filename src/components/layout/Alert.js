cb79c42ef8127253cdaae517a7737eacb19a14f7
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2020-10-22 17:33:14 +0300

    prettier

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,25 +1,25 @@
-import React, { useContext } from 'react'
-import AlertContext from '../../context/alert/alertContext';
+import React, { useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
 
 const Alert = () => {
-    const alertContext = useContext(AlertContext);
-    const {alert, remove} = alertContext;
-    
-   return (
-       alert !== null && (
-    <div className = {`alert alert-${alert.type}`}>
-        <i className="fas fa-info-circle">{alert.msg}</i>
-        <button onClick={remove} style={{float:'right', width:'30px'}}>X</button>
-    </div>
-        )
-    )
-}
+  const alertContext = useContext(AlertContext);
+  const { alert, remove } = alertContext;
 
+  return (
+    alert !== null && (
+      <div className={`alert alert-${alert.type}`}>
+        <i className="fas fa-info-circle">{alert.msg}</i>
+        <button onClick={remove} style={{ float: "right", width: "30px" }}>
+          X
+        </button>
+      </div>
+    )
+  );
+};
 
 export default Alert;
 
-
-// if (alert !==null) return ( 
+// if (alert !==null) return (
 //     (<div className = {`alert alert-${alert.type}`}>
 //      <i className="fas fa-info-circle">{alert.msg}</i>
 //      <button onClick={remove} style={{float:'right', width:'30px'}}>X</button>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,21 +1,29 @@
-import React from 'react'
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const UserItem = (props) => {
-
-   const {login , avatar_url } = props.user;
-   return (
-       <div className="card text-center">                
-           <img src={avatar_url} alt="" className="round-img" style={{width:'60px'}}/>
-           <h3>{login}</h3>
-           <div><Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">More</Link></div>
-       </div>
-   );
-}
+  const { login, avatar_url } = props.user;
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
+          More
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 UserItem.propTypes = {
-    user: PropTypes.object.isRequired
-}
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
